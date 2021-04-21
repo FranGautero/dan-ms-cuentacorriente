2a04fe35d06418cd76ca8efd66b876261e67923f
Francisco Gautero <francisco.gautero@gmail.com>
2021-04-20 23:17:32 -0300

    domain created

---
+++ src/main/java/domain/Efectivo.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Efectivo extends MedioPago{
+
+    private Integer nroRecibo;
+
+    public Integer getNroRecibo() {
+        return nroRecibo;
+    }
+
+    public void setNroRecibo(Integer nroRecibo) {
+        this.nroRecibo = nroRecibo;
+    }
+
+}
---
+++ src/main/java/domain/MedioPago.java
@@ -0,0 +1,20 @@
+package domain;
+
+public abstract class MedioPago {
+
+    protected Integer id;
+    protected String observacion;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getObservacion() {
+        return observacion;
+    }
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+}
