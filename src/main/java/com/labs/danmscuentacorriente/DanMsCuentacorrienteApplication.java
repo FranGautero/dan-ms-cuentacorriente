474c0bbd6284cf74eda32d6da3d4941a315862ce
Francisco Gautero <francisco.gautero@gmail.com>
2021-04-20 21:42:00 -0300

    Initial commit

---
+++ src/main/java/com/labs/danmscuentacorriente/DanMsCuentacorrienteApplication.java
@@ -0,0 +1,13 @@
+package com.labs.danmscuentacorriente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsCuentacorrienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsCuentacorrienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labs/danmscuentacorriente/DanMsCuentacorrienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.danmscuentacorriente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsCuentacorrienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
